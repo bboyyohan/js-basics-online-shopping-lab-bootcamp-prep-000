36ffe26b15da83b12b4a1e213a919ffcb60eac80
stupendous-patch-9345 <yo.park93@yahoo.com>
2019-09-25 23:41:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var cart = [];
 
 function getCart() {
+<<<<<<< HEAD
  return cart;
 }
 
@@ -14,10 +15,29 @@ function addToCart(item) {
   // console.log(itemObj);
   cart.push(itemObj);
   return itemObj.itemName + " has been added to your cart."
+=======
+  return cart;
+}
+
+
+function setCart(newCart) {	function setCart(newCart) {
+  cart = newCart;	  cart = newCart;
+}	}
+
+
+function addToCart(item) {
+  var price = Math.floor(Math.random() * 100) + 1;
+  var item_object = {};
+  item_object[item] = price
+  cart.push(item_object)
+  console.log(item + " has been added to your cart.");
+  return cart
+>>>>>>> da5161ceae7ccadad7ce22020660c01ed0e3b492
 }
 
 function viewCart() {
   if (cart.length === 0) {
+<<<<<<< HEAD
     return "Your shopping cart is empty."
   } else if (cart.length === 1) {
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`
@@ -65,3 +85,38 @@ function placeOrder(cardNumber) {
   }
 }
 
+=======
+    console.log("Your shopping cart is empty.")
+  } else {
+    var items = [];
+    for (var i = 0; i < cart.length; i++) {
+      for (var item in cart[i]) {
+        items.push(item + " at $" + cart[i][item])
+      }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
+  }
+}
+
+function removeFromCart(item) {
+  var itemInCart = false
+  for(var i = 0; i < cart.length; i++){
+    if (cart[i].hasOwnProperty(item)) {
+      itemInCart = true;
+      cart.splice(i, 1);
+    }
+  }
+  if (!itemInCart) {
+    console.log("That item is not in your cart.");
+  }
+  return cart
+}
+function placeOrder(creditCard) {
+  if (!creditCard) {
+    console.log("We don't have a credit card on file for you to place your order.");
+  } else {
+    console.log("Your total cost is $" + total() + ", which will be charged to the card " + creditCard + ".");
+    cart = [];
+  }
+}
+>>>>>>> da5161ceae7ccadad7ce22020660c01ed0e3b492
